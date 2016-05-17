1bfe597db7ff497f9e797c4f85b68d0080bc9e87
Emory Merryman <emory.merryman@gmail.com>
2016-05-17 09:25:33 +0000

    kludge for testing

--- user.sh
+++ user.sh
@@ -3,4 +3,5 @@
 (gpg --allow-secret-key- --import /opt/needlesslaser/private/secret.key || true ) &&
     (gpg2 --allow-secret-key- --import /opt/needlesslaser/private/secret.key || true ) &&
     node /opt/needlesslaser/c9sdk/server.js -w /home/"${@}"/workspace &&
+    sleep 1m &&
     true
