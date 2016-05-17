55128954d41789e5726604a66ee2fc8a26e0e00f
Emory Merryman <emory.merryman@gmail.com>
2016-05-17 10:21:28 +0000

    add which dep

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dnf update --assumeyes &&
-    dnf install --assumeyes git node make gcc python tar sudo pass &&
+    dnf install --assumeyes git node make gcc python tar which sudo pass &&
     dnf update --assumeyes &&
     dnf clean all &&
     chmod 0555 /opt/needlesslaser/private &&
