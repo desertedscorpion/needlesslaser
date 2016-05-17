b8a8e266e7adf1bbd12cb63099cdd4bdfe7ab80e
Emory Merryman <emory.merryman@gmail.com>
2016-05-17 08:50:51 +0000

    add user to run command

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "${LUSER} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${LUSER} &&
+adduser ${LUSER} &&
+    echo "${LUSER} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${LUSER} &&
     chmod 0444 /etc/sudoers.d/${LUSER} &&
     node /opt/needlesslaser/c9sdk/server.js &&
     true
