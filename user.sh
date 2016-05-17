49069bf1c4c069b33bbd70096e2fde7a0f53847b
Emory Merryman <emory.merryman@gmail.com>
2016-05-17 09:49:16 +0000

    can not access private files from user script and add post-commit script

--- user.sh
+++ user.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-(gpg --allow-secret-key- --import /opt/needlesslaser/private/secret.key || true ) &&
-    (gpg2 --allow-secret-key- --import /opt/needlesslaser/private/secret.key || true ) &&
-    node /opt/needlesslaser/c9sdk/server.js -w /home/"${@}"/workspace &&
+# (gpg --allow-secret-key- --import /opt/needlesslaser/private/secret.key || true ) &&
+#    (gpg2 --allow-secret-key- --import /opt/needlesslaser/private/secret.key || true ) &&
+    mkdir ${HOME}/workspace &&
+    git -C ${HOME}/workspace init &&
+    ln --symbolic --force /opt/needlesslaser/bin/post-commit ${HOME}/workspace/.git/hooks &&
+    node /opt/needlesslaser/c9sdk/server.js -w ${HOME}/workspace &&
     sleep 1m &&
     true
