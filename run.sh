2b2855013838f2312d450b7730ef733e2f187715
Emory Merryman <emory.merryman@gmail.com>
2016-05-17 09:30:21 +0000

    wtf

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 adduser --create-home ${LUSER} &&
     mkdir /home/${LUSER}/workspace &&
     echo "${LUSER} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${LUSER} &&
+    ls /etc/sudoers.d/${LUSER} &&
+    cat /etc/sudoers.d/${LUSER} &&
     chmod 0444 /etc/sudoers.d/${LUSER} &&
     su --login ${LUSER} --command "/opt/needlesslaser/sbin/user ${LUSER}"
     true
