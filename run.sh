cef5d4e06baf9c467c5454ad296fd781e7e913e0
Emory Merryman <emory.merryman@gmail.com>
2016-05-17 09:33:34 +0000

    temporarily deleting code that is not central

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ adduser --create-home ${LUSER} &&
     echo "${LUSER} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${LUSER} &&
     ls /etc/sudoers.d/${LUSER} &&
     cat /etc/sudoers.d/${LUSER} &&
-    chmod 0444 /etc/sudoers.d/${LUSER} &&
+#    chmod 0444 /etc/sudoers.d/${LUSER} &&
     su --login ${LUSER} --command "/opt/needlesslaser/sbin/user ${LUSER}"
     true
