f95e241307bae3c8977947089b689934c8931947
Emory Merryman <emory.merryman@deciphernow.com>
2016-05-21 18:47:36 -0400

    add ssh configuration

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+cp /opt/needlesslaser/private/id_rsa ${HOME}/.ssh/id_rsa &&
+chmod 0600 ${HOME}/.ssh/id_rsa &&
 node /opt/needlesslaser/c9sdk/server.js --listen 0.0.0.0 -w ${HOME}/workspace -p 8080 --auth ${USER}:${USER} &&
 true
--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@
 
 mkdir ${HOME}/workspace &&
 curl -L https://raw.githubusercontent.com/c9/install/master/install.sh | bash &&
+mkdir ${HOME}/.ssh &&
+chmod 0700 ${HOME}/.ssh &&
+cp /opt/needlesslaser/lib/config ${HOME}/.ssh/config &&
+chmod 0600 /${HOME}/.ssh/config &&
 true
