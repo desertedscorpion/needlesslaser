98bb1faaa11b5751cd70a223bed85789a8ca1204
Emory Merryman <emory.merryman@deciphernow.com>
2016-05-21 18:53:23 -0400

    add git

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+GIT_EMAIL="${1}" &&
+GIT_NAME="${2}" &&
+GIT_URL="${3}" &&
+GIT_BRANCH_PARENT="${4}" &&
+GIT_BRANCH_CHILD="${5}" &&
 cp /opt/needlesslaser/private/id_rsa ${HOME}/.ssh/id_rsa &&
 chmod 0600 ${HOME}/.ssh/id_rsa &&
+git config --global user.email "${GIT_EMAIL}" &&
+git config --global user.name "${GIT_NAME}" &&
+git -C ${HOME}/workspace remote add origin ${GIT_URL} &&
+git -C ${HOME}/workspace pull origin "${GIT_BRANCH_PARENT}" &&
+git -C ${HOME}/workspace checkout -b scratch-$(cat /dev/urandom | tr -dc 'A-Z0-9' | fold -w 4 | head -n 1)-$(echo "${GIT_BRANCH_CHILD}" | sed -e 's/ /_/g') &&
 node /opt/needlesslaser/c9sdk/server.js --listen 0.0.0.0 -w ${HOME}/workspace -p 8080 --auth ${USER}:${USER} &&
 true
--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-mkdir ${HOME}/workspace &&
 curl -L https://raw.githubusercontent.com/c9/install/master/install.sh | bash &&
+mkdir ${HOME}/workspace &&
+git -C ${HOME}/workspace init &&
 mkdir ${HOME}/.ssh &&
 chmod 0700 ${HOME}/.ssh &&
 cp /opt/needlesslaser/lib/config ${HOME}/.ssh/config &&
