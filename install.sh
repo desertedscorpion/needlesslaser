8393671045d1370e4e75195a54a6359e4889adfe
Emory Merryman <emory.merryman@deciphernow.com>
2016-05-22 18:18:46 +0000

    install xz

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 chmod 0500 /opt/needlesslaser/sbin/* &&
 chmod 0555 /opt/needlesslaser/bin/* &&
 dnf update --assumeyes &&
-dnf install --assumeyes git nodejs make python tar which bzip2 ncurses gmp-devel mpfr-devel libmpc-devel glibc-devel flex bison glibc-static zlib-devel gcc gcc-c++ sudo bash-completion pass gnupg gnupg2 nginx npm &&
+dnf install --assumeyes git nodejs make python tar which bzip2 ncurses gmp-devel mpfr-devel libmpc-devel glibc-devel flex bison glibc-static zlib-devel gcc gcc-c++ sudo bash-completion pass gnupg gnupg2 nginx npm xz &&
 /opt/needlesslaser/sbin/install-node 6.2.0 &&
 dnf update --assumeyes &&
 dnf clean all &&
