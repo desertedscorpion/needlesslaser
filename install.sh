cbc4c62d8fce859e95a3c9c48549a09701fa0297
Emory Merryman <emory.merryman@deciphernow.com>
2016-05-22 01:22:39 +0000

    add bash-autocompletion

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 chmod 0500 /opt/needlesslaser/sbin/* &&
 chmod 0555 /opt/needlesslaser/bin/* &&
 dnf update --assumeyes &&
-dnf install --assumeyes git nodejs make python tar which bzip2 ncurses gmp-devel mpfr-devel libmpc-devel glibc-devel flex bison glibc-static zlib-devel gcc gcc-c++ sudo &&
+dnf install --assumeyes git nodejs make python tar which bzip2 ncurses gmp-devel mpfr-devel libmpc-devel glibc-devel flex bison glibc-static zlib-devel gcc gcc-c++ sudo bash-completion &&
 dnf update --assumeyes &&
 dnf clean all &&
     mkdir /opt/needlesslaser/c9sdk &&
