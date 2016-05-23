ddcc5eb286fc18996a63cbda78553acec4abcdb5
Emory Merryman <emory.merryman@deciphernow.com>
2016-05-23 00:39:15 +0000

    wtf

--- init.sh
+++ init.sh
@@ -9,5 +9,6 @@ fi &&
 echo "${LUSER} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${LUSER} &&
 chmod 0440 /etc/sudoers.d/${LUSER} &&
 ln --symbolic --force /home/${LUSER}/nginx /etc/nginx &&
+echo  "${GIT_EMAIL}" "${GIT_NAME}" "${GIT_URL}" "${GIT_BRANCH_PARENT}" "${GIT_BRANCH_CHILD}" "${LDAP_USERNAME}" "${LDAP_PASSWORD}" "${LDAP_EMAIL}" "${GPG_KEY_ID}" "${PASS_GIT_URL}" &&
 su --login ${LUSER} /opt/needlesslaser/bin/run "${GIT_EMAIL}" "${GIT_NAME}" "${GIT_URL}" "${GIT_BRANCH_PARENT}" "${GIT_BRANCH_CHILD}" "${LDAP_USERNAME}" "${LDAP_PASSWORD}" "${LDAP_EMAIL}" "${GPG_KEY_ID}" "${PASS_GIT_URL}" &&
 true
