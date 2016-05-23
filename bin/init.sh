0ceb31789a1e051a40de24ab54ca33f5b5c3517c
Emory Merryman <emory.merryman@deciphernow.com>
2016-05-23 16:04:33 +0000

    do not pull pass master

--- bin/init.sh
+++ bin/init.sh
@@ -13,7 +13,7 @@ gpg --import-ownertrust /opt/needlesslaser/private/owner.trust &&
 pass init ${PASS_GPG_KEY_ID} &&
 pass git init &&
 pass git remote add origin ${PASS_GIT_URL} &&
-pass git pull origin master &&
-ln --symbolic --force /opt/needlesslaser/bin/post-commit.sh ${HOME}/.password-store/.git/hooks &&
+# pass git pull origin master &&
+ln --symbolic --force /opt/needlesslaser/bin/post-commit.sh ${HOME}/.password-store/.git/hooks/post-commit &&
 node /opt/needlesslaser/c9sdk/server.js --listen 0.0.0.0 -w ${HOME}/workspace -p 8080 --auth ${USER}:${USER} &&
 true
