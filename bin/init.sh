3ad10030e684daecf1584ddf0e5d1555b2d2308e
Emory Merryman <emory.merryman@deciphernow.com>
2016-05-23 16:29:35 +0000

    use fetch and rebase rather than pull ... no merge

--- bin/init.sh
+++ bin/init.sh
@@ -13,7 +13,8 @@ gpg --import-ownertrust /opt/needlesslaser/private/owner.trust &&
 pass init ${PASS_GPG_KEY_ID} &&
 pass git init &&
 pass git remote add origin ${PASS_GIT_URL} &&
-# pass git pull origin master &&
+pass git fetch origin master &&
+pass git rebase origin/master &&
 ln --symbolic --force /opt/needlesslaser/bin/post-commit.sh ${HOME}/.password-store/.git/hooks/post-commit &&
 node /opt/needlesslaser/c9sdk/server.js --listen 0.0.0.0 -w ${HOME}/workspace -p 8080 --auth ${USER}:${USER} &&
 true
