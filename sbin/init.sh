7d411f5b3481131ddbca27df9b7ccdd4b4c503a4
Emory Merryman <emory.merryman@deciphernow.com>
2016-05-23 13:32:51 +0000

    remove rubbish

--- sbin/init.sh
+++ sbin/init.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-if [ "user" != "${LUSER}" ]
-then
-    groupmod --new-name ${LUSER} user &&
-    usermod -d /home/${LUSER} -m -g ${LUSER} -l ${LUSER} user &&
-    true
-fi &&
-echo "${LUSER} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${LUSER} &&
-chmod 0440 /etc/sudoers.d/${LUSER} &&
-
-env > /opt/needlesslaser/environment &&
 su --login ${LUSER} /opt/needlesslaser/bin/init.sh  &&
 true
